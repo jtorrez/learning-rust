7196f6fd276f0d7d9b3c1513fe6947b5136d0835
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-09 18:05:13 -0700

    Add a second parameter for function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(100)
+    another_function(5, 12);
 }
 
-fn another_function (x: i32) {
-    println!("The value of x is {}", x);
+fn another_function (x: i32, y:i32) {
+    println!("x + y is {}", x+y);
 }
