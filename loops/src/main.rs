219effcf4f6ad65403fbc93f4bfc365b1551144e
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-09 18:24:41 -0700

    Add while loop implementation to loops project

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
