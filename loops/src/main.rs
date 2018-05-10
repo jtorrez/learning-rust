d442ddd42105f814fc0838c5ea92e012f443f997
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-09 18:26:41 -0700

    Modify loops project to use for loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
 
-    while number != 0 {
-        println!("{}!", number);
-
-        number = number - 1;
+    for element in a.iter() {
+        println!("the value is {}", element);
     }
-
-    println!("LIFTOFF!!!");
 }
