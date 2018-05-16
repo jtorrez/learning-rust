ee049e739baf93cf62244a178e201538ff7e0045
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 13:56:53 -0700

    Add can_hold method to Rectangle, modify main to use it

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,13 +10,19 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, r2: &Rectangle) -> bool {
+        self.width > r2.width && self.height > r2.height
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-        );
+    println!("The area of rect 1 is {}", rect1.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
