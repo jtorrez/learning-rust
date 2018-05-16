49f515778bbecd4d19945af5d4d222a699ac9bef
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 12:59:57 -0700

    Modify references to have example of dangling pointer

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,13 +1,12 @@
-// example of invalid simultaneous borrows of different kinds
+// example of dangling pointer that is caught at compile time
 fn main() {
-    let mut s = String::from("hello");
-
-    // Invalide because there are multiple references in the
-    // same scope, which COULD occur simultaneously and result
-    // in a data race. The race in this example is the mutation
-    // of r3 could occur before the immutable reference/borrow
-    // despite the ordering of the statements
-    let r1 = &s;
-    let r2 = &s;
-    let r3 = &mut s;
+    let reference_to_nothing = dangle();
 }
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+} // return value goes out of scope here so it is dropped
+  // but the function returns a reference to this dropped
+  // value, which would result in a dangling pointer
