c88d22dae7b00a256b516eb3466f2ec191fd2253
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 14:11:24 -0700

    Modify enums project to use enum with associated data syntax

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,27 +1,15 @@
 #[derive(Debug)]
-enum IpAddrVer {
-    V4,
-    V6,
-}
-
-#[derive(Debug)]
-struct IpAddr {
-    version: IpAddrVer,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        version: IpAddrVer::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    println!("Home is {:#?}", home);
+    println!("Home is {:?}", home);
 
-    let loopback = IpAddr {
-        version: IpAddrVer::V6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    println!("Loopback is {:#?}", loopback);
+    println!("Loopback is {:?}", loopback);
 }
