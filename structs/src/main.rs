3b5a1ecbd804ab4f9069448b583161a30e5c1fd2
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 13:31:30 -0700

    Add example of struct update syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,6 +17,18 @@ fn build_new_active_user(email: String, username: String) -> User {
     }
 }
 
+fn update_user(user1: User, email: String, username:String) -> User {
+    // uses struct update syntax, the ..user1 says to use the values
+    // for user1 for any struct fields not explicitly named in the new
+    // user struct
+    let user2 = User {
+        email,
+        username,
+        ..user1
+    };
+    user2
+}
+
 fn main() {
     let email = String::from("heybruv@example.com");
     let username = String::from("jerry_bob");
