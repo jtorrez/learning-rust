1e18fe17cc3c7a26023591b8a8c5c1877a9ceafd
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 13:37:01 -0700

    Add tuple struct examples and notes on ownership of struct data

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,9 +1,19 @@
+// the User struct is the owner of all it's data and the
+// memory of the data in it's field won't be dropped until
+// the struct is dropped. If the types of, say username for
+// example, were &str, then User would no longer own it's data
+// and the compiler would expect a lifetime parameter with
+// that field
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
 }
+
+// named tuples, act as tuples but now have defined type
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn build_new_active_user(email: String, username: String) -> User {
     // uses struct init shorthand
@@ -33,4 +43,10 @@ fn main() {
     let email = String::from("heybruv@example.com");
     let username = String::from("jerry_bob");
     build_new_active_user(email, username);
+
+    // black and origin now have different types
+    // due to using the tuple structs vs. them being
+    // untyped if we didn't use the tuple structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
