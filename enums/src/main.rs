7eff5ec329ef3db888aff0a64d8bab13e4863914
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 14:12:29 -0700

    Modify V4 IpAddr, all hail the power of enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,11 +1,11 @@
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
 
     println!("Home is {:?}", home);
 
