372f07b9d3f7a87eee38a042ed98e11aef24e404
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-16 14:21:01 -0700

    Add notes about enums and start of Option tutorial

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,6 +4,24 @@ enum IpAddr {
     V6(String),
 }
 
+// could define each of the enums as their own structs
+// but this gives the flexibility of being able to pass
+// a single Message type to a function/method rather
+// than having to have functions for each Message type
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// you can also implement methods on enums
+// impl Message {
+//     fn call(&self) {
+//     method body here
+//     }
+// }
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
 
@@ -12,4 +30,6 @@ fn main() {
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("Loopback is {:?}", loopback);
+
+    // let some_number = Some(5);
 }
