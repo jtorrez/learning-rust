30a395b5762891fb03055f957b363c84fef655e1
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-18 17:36:42 -0400

    Add match example for coins project

---
+++ coins/src/main.rs
@@ -0,0 +1,21 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let c = Coin::Penny;
+    let value = value_in_cents(c);
+    println!("The value of the coin is {}", value);
+}
