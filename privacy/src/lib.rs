a6a8e5bac73605e18ab2ffe26b14460b90abf2a2
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-19 23:06:45 -0400

    Add pub v. private example to privacy project

--- privacy/src/lib.rs
+++ privacy/src/lib.rs
@@ -1,3 +1,22 @@
+pub mod outermost {
+    pub fn middle_function () {}
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {}
+
+        fn secret_function () {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function(); // no errors, middle_function is public
+    outermost::middle_secret_function(); // error, middle_secret_function is private
+    outermost::inside::inner_function(); // error, inside module is private
+    outermost::inside::secret_function(); // error, inside module AND function are private
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
