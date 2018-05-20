b424904188ad4ca0f4e1c216cbc148aa06fe4cef
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-19 22:18:56 -0400

    Modify coins project to show enum innter value extraction

--- coins/src/main.rs
+++ coins/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // more
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
@@ -10,12 +17,15 @@ fn value_in_cents(coin: Coin) -> u32 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    let c = Coin::Penny;
+    let c = Coin::Quarter(UsState::Alabama);
     let value = value_in_cents(c);
     println!("The value of the coin is {}", value);
 }
