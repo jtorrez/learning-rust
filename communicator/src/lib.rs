801487f2dbac6be424a0b9afead241f875c33ca9
Jonathan Torrez <jonathan.a.torrez@gmail.com>
2018-05-19 22:36:26 -0400

    Add examples of modules to communicator project

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,3 +1,16 @@
+
+// name of the module is network
+mod network {
+    fn connect() {
+    }
+    // module is defined by mod, not by file
+    mod client {
+        fn connect() {
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
